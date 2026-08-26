5159fd676248e16f9099d40a2a554e03f39a293e
Chey <cloudheart15@gmail.com>
2026-08-26 16:19:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+{
+    username: "your_username",
+    character: "Character Name",
 
+    avatar:
+        "YOUR_PROFILE_IMAGE",
+
+    image:
+        "YOUR_FAN_ART_IMAGE",
+
+    likes: 42,
+
+    caption:
+        "Your caption goes here!",
+
+    comments: [
+        {
+            username: "another_character",
+            text: "This is so cool!"
+        },
+
+        {
+            username: "someone_else",
+            text: "I love this."
+        }
+    ],
+
+    time: "3 hours ago"
+}
